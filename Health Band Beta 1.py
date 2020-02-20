4d31df387da0f9670afd75a3244e23f7cfaae5d8
Rinkesh-Kumar <52789322+Rinkesh-Kumar@users.noreply.github.com>
2020-02-20 22:35:00 +0530

    Update Health Band Beta 1.py

--- Health Band Beta 1.py
+++ Health Band Beta 1.py
@@ -1,3 +1,4 @@
+''' The Sampling rate is 8 '''
 #Importing Libraries
 
 import numpy as np
@@ -11,9 +12,8 @@ state=Adxl335.Physical_Detection()
 import blynklib
 import Temperature
 import time
-auth_tokken='9AWNZz9pwrE_RpQq8hrX_moi_g1spJzk'
-#arunima-->auth_tokken='jd2eASXMvWd16IAUBJW9KKH15SImdMVQ'
-#dhruv--->auth_tokken='cP7wv9-j4TBEfZzaaguS0Kiqdo-vZWsG'
+#defining Token for Blynk
+auth_tokken='9AWNZz9pwrE_RpQq8hrX_moxxxxxxxx'
 blynk=blynklib.Blynk(auth_tokken)
 temp=Temperature.Temperature()
 #Defining the Butter Bandpass filter
@@ -33,12 +33,14 @@ red1=[]
 ir1=[]
 i=0
 while(True):
+    # Getting the temperature
     temp_c=temp.getTemperature()
     temp_f=temp_c*1.8+32
     print(temp_c)
     temp_c=round(temp_c,2)
     temp_f=round(temp_f,2)
     temp_r=str(temp_c)+'°C/'+str(temp_f)+'°F'
+    # Establishing connection with Blynk server
     blynk.run()
     blynk.virtual_write(2,temp_r)
     params=state.estimate_physical_state()
@@ -49,7 +51,8 @@ while(True):
         pass
     blynk.virtual_write(1,params[3][0])
     red, ir = m.read_fifo()
-    if red<=3000 and ir<=3000 and (params[3][0]=='Walking' or params[3][0]=='Running):
+    #Checking for no finger or motion 
+    if red<=3000 or ir<=3000 or (params[3][0]=='Walking' or params[3][0]=='Running):
         red1=[]
         ir1=[]
         print("No Heart beat detected")
@@ -58,7 +61,6 @@ while(True):
     elif red > 3000 and ir > 3000 and len(red1)<80 and len(ir1)<80 and params[3][0]=='Resting':
         red1.append(red)
         ir1.append(ir)
-        #print("{},{},{}".format(datetime.now(),red,ir))
         print("Wait I am calculating")
 
     elif len(red1)==80 and len(ir1)==80:
@@ -71,16 +73,14 @@ while(True):
 
         #Finding the Heart Rate peaks
         peaks_red=find_peaks(red_sample)
-        #print("Red Light HR is:->")
-        #print(len(peaks_red[0])*6)
         peaks_ir=find_peaks(ir_sample)
-        #print("IR Light HR is -->")
-        #print(len(peaks_ir[0])*6)
         print("Heart Rate is --->")
+        # Taking the average of both LEDs
         print((len(peaks_ir[0])*6+len(peaks_red[0])*6)//2)
         hr=str((len(peaks_ir[0])*6+len(peaks_red[0])*6)//2)+' bpm'
         blynk.virtual_write(3,hr)
         #Estimating SPO2
+        # Calculating the AC values of IR and Red PPG signal
         red_locmax=argrelextrema(red1,np.greater)
         red_locmin=argrelextrema(red1,np.less)
         ir_locmax=argrelextrema(ir1,np.greater)
@@ -97,11 +97,13 @@ while(True):
         for i in range (min(len(ir_locmax),len(ir_locmin))):
             if ir1[ir_locmax[i]]-ir1[ir_locmin[i]] > ir_ac:
                 ir_ac=ir1[ir_locmax[i]]-ir1[ir_locmin[i]]
+        # Calculating the DC value of IR and Red PPG signal
         red_dc=np.mean(red1)
         ir_dc=np.mean(ir1)
         num=red_ac*ir_dc
         den=ir_ac*red_dc
         r=num/den
+        # Formula for conversion of ratio to SpO2 percentage as given by MAX30102 Official Arduino Library
         spo2=(-45.060)*(r**2)+(30.054)*r+94.845
         print("Oxygen Content in blood is :")
         print(round(spo2,2))
